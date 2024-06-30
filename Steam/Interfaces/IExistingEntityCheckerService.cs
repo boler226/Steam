1c23eb247d2887b2074ae5c08de78f9a258b01fb
Illya <illamatviuk@gmail.com>
2024-06-30 21:51:38 +0300

    Add ExistingEntityCheckerService.

---
+++ Steam/Interfaces/IExistingEntityCheckerService.cs
@@ -0,0 +1,8 @@
+﻿namespace Steam.Interfaces
+{
+    public interface IExistingEntityCheckerService
+    {
+        Task<bool> IsCorrectCategoryId(int id, CancellationToken cancellationToken);
+        Task<bool> IsCorrectGameId(int id, CancellationToken cancellationToken);
+    }
+}
---
+++ Steam/Services/ExistingEntityCheckerService.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Steam.Data;
+using Steam.Interfaces;
+
+namespace Steam.Services
+{
+    public class ExistingEntityCheckerService(
+        AppEFContext context
+        ) : IExistingEntityCheckerService
+    {
+        public async Task<bool> IsCorrectCategoryId(int id, CancellationToken cancellationToken) =>
+            await context.Categories.AnyAsync(c => c.Id == id, cancellationToken);
+
+        public async Task<bool> IsCorrectGameId(int id, CancellationToken cancellationToken) =>
+            await context.Games.AnyAsync(g => g.Id == id, cancellationToken);
+    }
+}
