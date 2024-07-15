33a527bf19ef35478b115a65f7a2d96fb1b321f4
Illya <illamatviuk@gmail.com>
2024-07-15 18:02:29 +0300

    Added show list in GamesListPage.

--- SteamFront/src/components/games/GamesListPage.tsx
+++ SteamFront/src/components/games/GamesListPage.tsx
@@ -1,4 +1,4 @@
-import {Flex, Layout, Card } from "antd";
+import {Flex, Layout } from "antd";
 import './style/style.css';
 import {useEffect, useState} from "react";
 import { IGameItem } from "./types.ts";
@@ -10,6 +10,7 @@ const { Content } = Layout;
 const GamesListPage = () => {
 
     const [data, setData] = useState<IGameItem[]>([]);
+    const [game, setGame] = useState<IGameItem>();
 
     useEffect(() => {
         fetchGames();
@@ -26,51 +27,65 @@ const GamesListPage = () => {
         console.log("game click", gameItem);
     };
 
+    const handleMouseEnter = (gameItem: IGameItem) => {
+        setGame(gameItem);
+    }
+
     return(
         <Layout style={{ background: 'linear-gradient(to bottom, rgba(42,71,94,1), rgba(27, 40, 56, 0.1))' }}>
-                <div className="card" >
-                    <Flex  align='center' style={{ height: '100%' }}>
-                        <img className="game-card-image" alt="ff"
-                             src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTJANR1OZtB3WTef1I80MZGu8t93BEfj5BfXg&s"/>
-                        <Flex justify='space-between' align='center' style={{width:'400px', padding:'10px'}}>
-                            <Flex vertical justify='space-between'>
-                                <p className="card-title">Rocket League</p>
-                                <p className="card-categories">Cars, Road, Simulator...</p>
-                            </Flex>
-                            <p className="card-price">22$</p>
+            <Content className="games-container">
+                <div className="games-card-list">
+                    <Flex  justify='center' gap={25}>
+                        <Flex vertical justify='space-around' align='center' gap={10} style={{padding: '10px 0'}}>
+                            {data && data.length > 0 && data.slice(0, 10).map((item) => (
+                                <div key={item.id}>
+                                    <div className="card"
+                                         onClick={() => handleGameClick(item)}
+                                         onMouseEnter={() => handleMouseEnter(item)}>
+                                        <div className="left-card">
+                                            <Flex align='center' style={{height: '100%'}}>
+                                                <img className="game-card-image" alt={item.name}
+                                                     src={`http://localhost:5002/images/${ImageSizes.extraLarge}_${item.images[0]}`}/>
+                                                <Flex justify='space-between' align='center'
+                                                      style={{width: '400px', padding: '10px'}}>
+                                                    <Flex vertical justify='space-around' style={{height: '70px'}}>
+                                                        <div className="card-title">{item.name}</div>
+                                                        <div className="card-categories">Cars, Road,
+                                                            Simulator...</div>
+                                                    </Flex>
+                                                    <p className="card-price">{item.price}$</p>
+                                                </Flex>
+                                            </Flex>
+                                        </div>
+                                    </div>
+                                </div>
+                            ))}
                         </Flex>
+
+                        <div className="right-card">
+                            <Flex vertical justify='space-around' align='start' style={{padding: '0 20px'}}>
+                                <p className="card-title-right">{game?.name}</p>
+                                <div className="card-rating">
+                                    Усі рецензії користувачів:<br/>
+                                    дуже схвальна (...)
+                                </div>
+                                <Flex justify='space-around' gap={5} style={{padding: '10px 0', maxWidth: '260px', overflow: 'hidden'}}>
+                                    {game?.categories.map((item) =>
+                                        <div className="card-categories-right" key={item.id}>
+                                            {item.name}
+                                        </div>
+                                    )}
+                                </Flex>
+                                <Flex vertical justify='space-around'>
+                                    {game?.images.slice(0, 4).map((item) => (
+                                        <img key={item.id} className="card-image-right" alt={item.name}
+                                             src={`http://localhost:5002/images/${ImageSizes.extraLarge}_${item}`}/>
+                                    ))}
+                                </Flex>
+                            </Flex>
+                        </div>
                     </Flex>
                 </div>
-            <Content className="games-container">
-                <Flex vertical justify='space-around' align='center' gap={50}>
-                {data && data.length > 0 && data.map((item) => (
-                        <div key={item.id}>
-                            <div className="games-card-list">
-                                <div>
-                                    <Flex vertical justify='space-around' align='center' gap={25}>
-                                        <div className="game-card"
-                                             onClick={() => handleGameClick(item)}>
-                                            <Card size='small'>
-                                                <Flex justify='space-around' align='center'>
-                                                    <img className="game-card-image" alt={item.name}
-                                                         src={`http://localhost:5002/images/${ImageSizes.extraLarge}_${item.images[0].name}`}/>
-                                                    <Flex vertical justify='space-around'>
-                                                        <p>{item.name}</p>
-                                                        <p>Categories...</p>
-                                                    </Flex>
-                                                    <p>{item.price}</p>
-                                                </Flex>
-                                            </Card>
-                                        </div>
-                                    </Flex>
-                                </div>
-                                <div>
-                                    {/* Інший контент */}
-                                </div>
-                            </div>
-                        </div>
-                    ))}
-                </Flex>
             </Content>
         </Layout>
     )
