23af26773d1e274dd03986b697ceb4ca8d7fc1e1
Illya <illamatviuk@gmail.com>
2024-07-16 20:59:56 +0300

    Added PaginationService.

---
+++ Steam/Models/Pagination/PageViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Steam.Models.Pagination
+{
+    public class PageViewModel<T>
+    {
+        public IEnumerable<T> Data { get; set; }
+        public int PageAvailable { get; set; }
+        public int ItemsAvailable { get; set; }
+    }
+}
---
+++ Steam/Services/PaginationServices/Base/PaginationService.cs
@@ -0,0 +1,63 @@
+﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using Microsoft.EntityFrameworkCore;
+using Steam.Interfaces;
+using Steam.Models.Pagination;
+
+namespace Steam.Services.PaginationServices.Base
+{
+    public abstract class PaginationService <EntityType, EntityViewModelType, PaginationViewModelType>(
+        IMapper mapper
+        ) : IPaginationService<EntityViewModelType, PaginationViewModelType> where PaginationViewModelType : PaginationViewModel
+    {
+        public async Task<PageViewModel<EntityViewModelType>> GetPageAsync(PaginationViewModelType model)
+        {
+            if (model.PageSize is not null && model.PageIndex is null)
+                throw new Exception("PageIndex is required if PageSize is initialized");
+
+            if (model.PageIndex < 0)
+                throw new Exception("PageIndex less than 0");
+
+            if (model.PageSize < 1)
+                throw new Exception("PageSize is invalid");
+
+            var query = GetQuery();
+
+            query = FilterQuery(query, model);
+
+            int count = await query.CountAsync();
+
+            int pagesAvailable;
+
+            if (model.PageSize is not null)
+            {
+                pagesAvailable = (int)Math.Ceiling(count / (double)model.PageSize);
+                query = query
+                    .Skip((int)model.PageIndex! * (int)model.PageSize)
+                    .Take((int)model.PageSize);
+            }
+            else
+            {
+                pagesAvailable = (count > 0) ? (1) : (0);
+            }
+
+            var data = await MapAsync(query);
+
+            return new PageViewModel<EntityViewModelType>
+            {
+                Data = data,
+                PageAvailable = pagesAvailable,
+                ItemsAvailable = count
+            };
+        }
+
+        protected abstract IQueryable<EntityType> GetQuery();
+        protected abstract IQueryable<EntityType> FilterQuery(IQueryable<EntityType> query, PaginationViewModelType paginationViewModel);
+        protected virtual async Task<IEnumerable<EntityViewModelType>> MapAsync(IQueryable<EntityType> query)
+        {
+            return await query
+                .ProjectTo<EntityViewModelType>(mapper.ConfigurationProvider)
+                .ToArrayAsync();
+        }
+    }
+}
