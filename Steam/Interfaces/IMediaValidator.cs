424daa7cb3da1896f4755a17052559aade21d8cb
Illya <illamatviuk@gmail.com>
2024-08-04 14:19:39 +0300

    Added MediaValidator.

---
+++ Steam/Interfaces/IMediaValidator.cs
@@ -0,0 +1,7 @@
+﻿namespace Steam.Interfaces
+{
+    public interface IMediaValidator
+    {
+        Task<bool> IsValidMediaAsync(IFormFile media, CancellationToken cancellationToken);
+    }
+}
---
+++ Steam/Services/MediaValidator.cs
@@ -0,0 +1,28 @@
+﻿using Steam.Interfaces;
+
+namespace Steam.Services
+{
+    public class MediaValidator(
+        IImageValidator imageValidator,
+        IVideoValidator videoValidator
+        ) : IMediaValidator
+    {
+        public async Task<bool> IsValidMediaAsync(IFormFile media, CancellationToken cancellationToken)
+        {
+            var extension = Path.GetExtension(media.FileName).ToLowerInvariant();
+
+            if (IsImage(extension))
+            {
+                return await imageValidator.IsValidImageAsync(media, cancellationToken);
+            }
+            else if (IsVideo(extension))
+            {
+                return await videoValidator.IsValidVideoAsync(media, cancellationToken);
+            }
+
+            return false;
+        }
+        private bool IsImage(string extension) => new[] { ".jpg", ".jpeg", ".png", ".webp", ".gif" }.Contains(extension);
+        private bool IsVideo(string extension) => new[] { ".mp4", ".avi", ".mov", ".mkv" }.Contains(extension);
+    }
+}
