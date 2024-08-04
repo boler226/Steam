a2d4a80a5c69943670ceaf53b4987228ba53fbf4
Illya <illamatviuk@gmail.com>
2024-08-04 13:49:08 +0300

    Added VideoValidator.

---
+++ Steam/Interfaces/IVideoValidator.cs
@@ -0,0 +1,10 @@
+﻿namespace Steam.Interfaces
+{
+    public interface IVideoValidator
+    {
+        Task<bool> IsValidVideoAsync(IFormFile video, CancellationToken cancellationToken);
+        Task<bool> IsValidNullPossibleVideoAsync(IFormFile video, CancellationToken cancellationToken);
+        Task<bool> IsValidVideosAsync(IEnumerable<IFormFile> videos, CancellationToken cancellationToken);
+        Task<bool> IsValidNullPossibleVideosAsync(IEnumerable<IFormFile> videos, CancellationToken cancellationToken);
+    }
+}
